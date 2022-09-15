b289de7c65985b93a37f9ee8476a6dbfe85d4741
residualeffect <20237347+residualeffect@users.noreply.github.com>
2022-09-15 00:56:32 -0700

    Fix issue where an observable value could change between the time useObservable is called and when observable is subscribed to

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## [1.1.0] - 2022-09-15
+### Fixed
+* Detect when an observable value has changed between the time when the useObservable hook was invoked and when the subscription was created, and trigger react as expected
+
 ## [1.0.0] - 2022-03-31
 ### Added
 * Initial Release, with support for react 16.8+
--- src/useObservable.ts
+++ src/useObservable.ts
@@ -3,6 +3,16 @@ import type { ReadOnlyObservable } from "@residualeffect/reactor";
 
 export function useObservable<T>(observable: ReadOnlyObservable<T>): T {
 	const [, triggerReact] = useReducer((x: number) => x + 1, 0);
-	useLayoutEffect(() => observable.Subscribe(triggerReact), [observable]);
-	return observable.Value;
+	const initialValue = observable.Value;
+
+	useLayoutEffect(() => {
+		observable.Subscribe(triggerReact);
+
+		// In case the observable value has changed between the time when the initial value was retrieved and when the subscription was created
+		if (!observable.IsEqualTo(initialValue)) {
+			triggerReact();
+		}
+	}, [observable]);
+
+	return initialValue;
 }
