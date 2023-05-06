f1e9d18b94a482e57bea508e2a788b22501233cb
residualeffect <20237347+residualeffect@users.noreply.github.com>
2023-05-05 21:01:57 -0700

    Enhance test coverage to verify that observable mutations during the commit phase are handled

--- tests/useObservable.test.tsx
+++ tests/useObservable.test.tsx
@@ -4,15 +4,15 @@ import { act, cleanup, render, screen } from "@testing-library/react";
 import { useObservable } from "../src/useObservable";
 import { Observable } from "@residualeffect/reactor";
 
-const Component: React.FC<{ id: Observable<string> }> = (props) => {
-	const id = useObservable(props.id);
-
-	return (
-		<p data-testid="id-field">{id}</p>
-	);
-};
-
 test("should re-render component when observable value is changed", () => {
+	const Component: React.FC<{ id: Observable<string> }> = (props) => {
+		const id = useObservable(props.id);
+
+		return (
+			<p data-testid="id-field">{id}</p>
+		);
+	};
+
 	const o = new Observable("foo");
 
 	render(<Component id={o} />);
@@ -25,6 +25,35 @@ test("should re-render component when observable value is changed", () => {
 		o.Value = "bar";
 	});
 	expect(element.innerHTML).toStrictEqual("bar");
+	expect(o.SubscriptionCount).toStrictEqual(1);
+
+	cleanup();
+	expect(o.SubscriptionCount).toStrictEqual(0);
+});
+
+test("should re-render component when legacy observable value is modified during react commit phase", () => {
+	const ComponentWithInlineUpdate: React.FC<{ id: Observable<string> }> = (props) => {
+		const id = useObservable(props.id);
+
+		props.id.Value = "Test";
+
+		return (
+			<p data-testid="id-field">{id}</p>
+		);
+	};
+
+	const o = new Observable("foo");
+
+	render(<ComponentWithInlineUpdate id={o} />);
+	const element = screen.getByTestId("id-field");
+
+	expect(element.innerHTML).toStrictEqual("Test");
+	expect(o.SubscriptionCount).toStrictEqual(1);
+
+	act(() => {
+		o.Value = "bar";
+	});
+	expect(element.innerHTML).toStrictEqual("Test");
 	expect(o.SubscriptionCount).toStrictEqual(1);
 
 	cleanup();
--- tests/useObservableLegacy.test.tsx
+++ tests/useObservableLegacy.test.tsx
@@ -4,15 +4,15 @@ import { act, cleanup, render, screen } from "@testing-library/react";
 import { useObservableLegacy } from "../src/useObservableLegacy";
 import { Observable } from "@residualeffect/reactor";
 
-const Component: React.FC<{ id: Observable<string> }> = (props) => {
-	const id = useObservableLegacy(props.id);
-
-	return (
-		<p data-testid="id-field">{id}</p>
-	);
-};
-
 test("should re-render component when legacy observable value is changed", () => {
+	const Component: React.FC<{ id: Observable<string> }> = (props) => {
+		const id = useObservableLegacy(props.id);
+
+		return (
+			<p data-testid="id-field">{id}</p>
+		);
+	};
+
 	const o = new Observable("foo");
 
 	render(<Component id={o} />);
@@ -25,6 +25,36 @@ test("should re-render component when legacy observable value is changed", () =>
 		o.Value = "bar";
 	});
 	expect(element.innerHTML).toStrictEqual("bar");
+	expect(o.SubscriptionCount).toStrictEqual(1);
+
+	cleanup();
+	expect(o.SubscriptionCount).toStrictEqual(0);
+});
+
+
+test("should re-render component when legacy observable value is modified during react commit phase", () => {
+	const ComponentWithInlineUpdate: React.FC<{ id: Observable<string> }> = (props) => {
+		const id = useObservableLegacy(props.id);
+
+		props.id.Value = "Test";
+
+		return (
+			<p data-testid="id-field">{id}</p>
+		);
+	};
+
+	const o = new Observable("foo");
+
+	render(<ComponentWithInlineUpdate id={o} />);
+	const element = screen.getByTestId("id-field");
+
+	expect(element.innerHTML).toStrictEqual("Test");
+	expect(o.SubscriptionCount).toStrictEqual(1);
+
+	act(() => {
+		o.Value = "bar";
+	});
+	expect(element.innerHTML).toStrictEqual("Test");
 	expect(o.SubscriptionCount).toStrictEqual(1);
 
 	cleanup();
